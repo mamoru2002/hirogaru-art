a7fef65d07d08ed2c477ad6d2e27d410d4ea7650
樫内守 <kashiuchi@kashiuchimamorunoMacBook-Air-2.local>
2024-11-15 21:37:36 +0900

    UserControllerの基本機能追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,42 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update]
+  before_action :authenticate_user!, except: [:show, :index]
+  before_action :ensure_correct_user, only: [:edit, :update]
+
+  def index
+    @users = User.all
+  end
+
   def show
+    # @userは before_action :set_user で設定済み
   end
 
   def edit
+    # @userは before_action :set_user で設定済み
   end
 
   def update
+    if @user.update(user_params)
+      redirect_to @user, notice: 'プロフィールを更新しました'
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
-  def index
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:username, :bio, :website, 
+                                :twitter, :instagram, :facebook, :avatar)
+  end
+
+  def ensure_correct_user
+    unless @user == current_user
+      redirect_to users_path, alert: '他のユーザーの情報は編集できません'
+    end
   end
 end
