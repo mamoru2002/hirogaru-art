eb1ef35ddd96cf705e8b92574026950e9c660a9e
樫内守 <kashiuchi@kashiuchimamorunoMacBook-Air-2.local>
2024-11-15 15:41:48 +0900

    プロフィール項目とActive Storageを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_11_14_084659) do
+ActiveRecord::Schema[7.2].define(version: 2024_11_15_063838) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -23,6 +23,11 @@ ActiveRecord::Schema[7.2].define(version: 2024_11_14_084659) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "username"
+    t.text "bio"
+    t.string "website"
+    t.string "twitter"
+    t.string "instagram"
+    t.string "facebook"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["username"], name: "index_users_on_username", unique: true
