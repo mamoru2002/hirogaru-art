a3ab65e318996ee6493022161bd9c02b35a5c3ed
樫内守 <kashiuchi@kashiuchimamorunoMacBook-Air-2.local>
2024-11-18 10:40:01 +0900

    ルーティングを修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,17 @@
 Rails.application.routes.draw do
-  # ユーザー関連（プロフィール、一覧など）
-  resources :users, only: [:index, :show, :edit, :update]
   # deviseのユーザー認証関連
   devise_for :users
+
   # ホーム画面の設定
   root "home#index"
 
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+  # ユーザー関連（プロフィール、一覧など）
+  resources :users, only: [:index, :show, :edit, :update]
 
   # Render dynamic PWA files from app/views/pwa/*
   get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  # root "posts#index"s
 end
